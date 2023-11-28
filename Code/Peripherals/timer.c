b673846995cc3eadcd95061f2c03da10e2b8b8c2
EnriqueMercader <149925325+EnriqueMercader@users.noreply.github.com>
2023-11-27 23:08:45 -0600

    Update timer.c

--- Code/Peripherals/timer.c
+++ Code/Peripherals/timer.c
@@ -12,8 +12,8 @@ void setTimer0(int phaseFrequency){
     while ((timerLoadValue < 0) || (timerLoadValue > 254));         // Checks if the calculated value is within the register's capacity
     TMR0H    = timerLoadValue;                                      // Load the value calculated to obtain the desired interrupt frequency
     TMR0L = 0;
-    T0CON1 = (0b01000000 | prescalerSelectBits);                     // Enables timer 0 in 8 bit mode with the calculated prescaler
-    PIR0bits.TMR0IF = 0;    // Clear Interrupt flag before enabling the interrupt
-    PIE0bits.TMR0IE = 1;    // Enabling TMR0 interrupt.
-    T0CON0 = 0b10000000;  // T0OUTPS 1:2; T0EN enabled; T016BIT 8-bit; 
+    T0CON1 = (0b01000000 | prescalerSelectBits);                         // Enables timer 0 in 8 bit mode with the calculated prescaler
+    PIR0bits.TMR0IF = 0;                                                // Clear Interrupt flag before enabling the interrupt
+    PIE0bits.TMR0IE = 1;                                                // Enabling TMR0 interrupt.
+    T0CON0 = 0b10000000;                                                // T0OUTPS 1:2; T0EN enabled; T016BIT 8-bit; 
 }
